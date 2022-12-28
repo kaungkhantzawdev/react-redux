28693e180aeaa0a7700ef6bd369907a64558ce29
mgkaungkhantzaw <kaungkhantzaw2001326@gmail.com>
2022-12-28 13:17:36 +0630

    dev

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import CakeContainer from './components/cake/cakeContainer';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import HookCakeContainer from './components/cake/HookCakeContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
       <CakeContainer/>
+      <HookCakeContainer/>
       </div>
     </Provider>
   );
